0e014abb863800a25649fc3893d0d81d8140d082
yuki-toida <yuki.toidax@gmail.com>
2018-05-05 15:57:33 +0900

    setup gin

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"key": "value"})
+	})
+	router.Run()
+}
