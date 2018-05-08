16c19a50c7741c756fdd557ec76e440f6e0887b4
yuki-toida <yuki.toidax@gmail.com>
2018-05-08 21:24:41 +0900

    MultiTemplate

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,8 +9,12 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("ui/templates/**/*")
+
 	router.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"key": "value"})
+		html := template.Must(template.ParseFiles("ui/templates/layout/main.html", "ui/templates/home/index.html"))
+		router.SetHTMLTemplate(html)
+		context.HTML(http.StatusOK, "main.html", nil)
 	})
 	router.Run()
 }
